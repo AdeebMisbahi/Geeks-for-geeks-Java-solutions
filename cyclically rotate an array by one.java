6ae6e7ebab57533c7f31530a07835b2f84c059db
Adeeb Misbahi <130211733+AdeebMisbahi1@users.noreply.github.com>
2023-06-23 22:12:09 +0530

    Update cyclically rotate an array by one.java

--- cyclically rotate an array by one.java
+++ cyclically rotate an array by one.java
@@ -50,3 +50,22 @@ class Compute {
         
     
 }
+
+
+
+
+
+// this is one more way
+
+
+    public void rotate(int arr[], int n)
+    {
+       int i = 0, j = arr.length - 1;
+        while(i != j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+        }
+    }
